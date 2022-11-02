10d83d7001ad6a97e7c53d36f0dd700d2dc6211c
Will S <fitnesswill13@gmail.com>
2022-11-02 16:07:21 -0700

    filled out models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,20 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -4,10 +4,19 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-
-// Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+Product.belongsToMany(ProductTag, {
+  foreignKey: 'product_id',
+});
+Tag.belongsToMany(ProductTag, {
+  foreignKey: 'tag_id',
+});
 // Products belongToMany Tags (through ProductTag)
 
 // Tags belongToMany Products (through ProductTag)
